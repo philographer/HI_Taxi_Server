ae8728ab9c25063f593e34711b1e402a1e1b6300
yuhogyun <yoohoogun114@naver.com>
2016-09-24 15:07:40 +0900

    structure

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
+'use strict';
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var pg = require('pg');
+var Sequelize = require('sequelize');
+const DbService = require('./lib/service/dbService');
+const dbService = new DbService({
+  dbName: 'hitaxi',
+  dbLogging: 'true',
+  dbUsername: 'yuhogyun',
+  dbPassword: 'gkdlxortl',
+  hostName: 'hitaxi.ctabe3wb6pkc.ap-northeast-2.rds.amazonaws.com'
+});
+var cors = require('cors');
+//console.log(dbService.getSequelize() == dbService);
 
+exports.dbService = dbService;
+
+//console.log(exports.dbService);
+//console.log(module);
 var routes = require('./routes/index');
-var users = require('./routes/users');
+
 
 var app = express();
-var Cors = require('cors');
+app.use(cors({
+  origin: '*'
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
-
-//External lib
-app.use(Cors());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -26,11 +42,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', routes);
-app.use('/users', users);
 
-// catch 404 and forward to error handler
+const DbModel = require('./lib/service/dbModel');
+
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
@@ -61,5 +76,10 @@ app.use(function(err, req, res, next) {
   });
 });
 
-
 module.exports = app;
+
+//console.log(module);
+
+//DB Init
+dbService.sync();
+
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
+/**
+ * Created by yuhogyun on 2016. 9. 24..
+ */
+var app = require('../app');
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.status(200).json({message: "hello world"});
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/hello', function(req, res){
+  app.dbService.createUser(req.body['facebookId'], req.body['username']).then(function(){
+    res.status(200).json({message: "회원가입 성공"});
+  }).catch(function(error){
+    console.log(error.message);
+  });
+
+  app.dbService.hello();
+
 });
 
 module.exports = router;
+
